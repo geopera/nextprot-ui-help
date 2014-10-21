e4ef8679a88b737c9f6232fb3b868cbfd924c54f
olivier <olivier.evalet@isb-sib.ch>
2014-10-21 16:46:35 +0200

    deploy a new version

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,20 @@
 (function (ng, undefined) {'use strict';
-	angular.module('main', ['ngResource','npHelp']);
+	angular.module('main', ['ngResource','npHelp'])
+
+	.config(function($routeProvider, $locationProvider, $provide) {
+		// Use the bang prefix for Google ajax crawlability
+		// https://developers.google.com/webmasters/ajax-crawling/docs/specification?csw=1
+
+
+		// Hashbang in HTML5 Mode
+		$locationProvider.html5Mode(false);
+		$locationProvider.hashPrefix('!');
+
+		$routeProvider
+			.when('/', { templateUrl: "html/np-help.md.html" })
+			.when('/faq', { templateUrl: "html/np-help.md.html" })
+			.when('/doc/:entity', { templateUrl: "html/np-help.element.html"})
+			.otherwise({ redirectTo: '/' });
+		
+	});  	
 })(angular);
