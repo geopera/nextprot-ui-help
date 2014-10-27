cc503d1958e54d963275161bd7e5b092973eaed2
olivier <olivier.evalet@isb-sib.ch>
2014-10-27 17:19:09 +0100

    deploy a new version

--- dist/np-help.js
+++ dist/np-help.js
@@ -1,31 +1,46 @@
-(function (ng) {
-    'use strict';
-    ng.module('npHelp',['ngRoute']).constant('settings', {
-    	baseUrl:window.BASE_SERVER,
-    	helpUrl:window.HELP_URL||'/nextprot-api/rdf/help/type/all.json',
-    	home:window.HOME||'home.md'
-    });  
+(function (ng) {'use strict';
+    var defaultSettings={
+        helpUrl:'/nextprot-api/rdf/help/type/all.json',
+        root:'',
+        pages:['faq','home','entity/']
+    }
+    ng.module('npHelp',['ngRoute']).constant('settings', angular.extend(defaultSettings,npHelpSettings));  
 })(angular);
 
 (function (ng, undefined) {'use strict';
-
-    ng.module('npHelp').controller('HelpCtrl', ['$scope', '$location','rdfHelp','settings','$routeParams', 
-        function ($scope, $location, rdfHelp, settings, $routeParams) {
-            
+    ng.module('npHelp').controller('HelpCtrl', ['$scope', '$location','rdfHelp','settings','$route','$log', 
+        function ($scope, $location, rdfHelp, settings, $route, $log) {
+            //
+            // simple helper to get markdown file from url
+            function parseMdFile(){
+                var page=$location.path().substring(1)
+                if(page===settings.root){
+                    return settings.pages[0]+'.md'
+                }
+                for(var i in settings.pages){
+                    if(page.indexOf(settings.pages[i])!==-1){
+                        return page+'.md'
+                    }
+                }
+            }
+            $scope.entityName=$route.current&&$route.current.params.entity||'';
+            $scope.entity={}
             $scope.settings=settings;
-            $scope.mdFile=false;
+            $scope.mdFile=parseMdFile();
+            $scope.rdfHelp=rdfHelp;
+
 
             //
             // update entity documentation 
             $scope.$on('$routeChangeStart', function(event, next, current) { 
-                $scope.mdFile=$location.path().substring(1)
-                if($scope.mdFile=='')$scope.mdFile=settings.home;
-                $scope.mdFile+='.md'
-                console.log("md",$scope.mdFile)
+                $scope.mdFile=parseMdFile()
                 if(next&&next.params&&next.params.entity){
                     $scope.entity=$scope.getActiveElement(next.params.entity)
                     $scope.entityName=next.params.entity;
                 }
+                $log.info("mdFile",$scope.mdFile)
+                $log.info("entityName",$scope.entityName)
+                $log.info("entity",$scope.entity)
             });  
 
             $scope.getActiveElement=function(entity){
@@ -40,36 +55,49 @@
                 return $scope.entityName===name
             }  
 
-            // load on initial 
-            $scope.rdfHelp=rdfHelp.query()
+            // load on init 
+            rdfHelp.query().$promise.then(function(help){
+                $scope.entity=$scope.getActiveElement($scope.entityName)
+            })
         }
     ]);
 })(angular);
 
-(function (ng, undefined) {
-    'use strict';
+(function (ng, undefined) { 'use strict';
     ng.module('npHelp')
-        .factory('rdfHelp', ['$resource','settings',
-        function($resource, settings) {
-            console.log(settings,(settings.baseUrl||'') + settings.helpUrl)
-            var $dao={
-                rdfHelp:$resource((settings.baseUrl||'') + settings.helpUrl)   
-            }
+        .factory('rdfHelp', ['$resource','$q','settings',
+        function($resource, $q, settings) {
             
 
             var Help=function(){
+                this.$dao={
+                    rdfHelp:$resource((settings.baseUrl||'') + settings.helpUrl)   
+                }
+
+                this.ready=false;
+
+                //
+                // wrap promise to this object
+                this.$promise=$q.when(this)
             }
 
             Help.prototype.query=function(){
                 var self=this;
-                return $dao.rdfHelp.query(null, function (data) {
-                    angular.extend(self,data);
-                }); 
+
+                // is emtpy
+                if(!this.ready){                
+                    this.$promise=self.$dao.rdfHelp.query().$promise;
+    
+                    this.$promise.then(function (data) {
+                        angular.extend(self,data);
+                        self.ready=true;
+                    }); 
+                }
+                return this;
             }
  
             return new Help()
         }
-
     ]); 
 })(angular);
 
@@ -80,14 +108,14 @@
             return {
                 restrict: 'A',
                 transclude: true,
-                replace: true,
-                templateUrl: 'np-help.template.html',
+                templateUrl: 'html/np-help.element.html',
                 link: function (scope, element, attr, ctrl) {
                 }
             };
         }])
 
-        .directive("markdown", function ($compile, $http, $parse, $timeout) {
+        .directive("markdown", ['$compile', '$http', '$parse', '$timeout', 
+          function ($compile, $http, $parse, $timeout) {
             var converter = new Showdown.converter();
             return {
                 restrict: 'E',
@@ -120,7 +148,7 @@
 
                 }
             };
-        })
+        }])
 
         .filter('trusted', ['$sce', function ($sce) {
             return function(url) {
@@ -143,6 +171,6 @@
 })(angular);
 
 angular.module('npHelp').run(['$templateCache', function ($templateCache) {
-	$templateCache.put('html/np-help.element.html', '<div class="row offset1"> <div class="row"> <section id="{{entity.typeName}}"> <h2> {{entity.typeName|cleanType}}  <div ng-if="entity.values.length> 0" class="btn-group"> <button class="btn dropdown-toggle" data-toggle="dropdown">Values<span class="caret"></span> </button> <ul class="dropdown-menu"> <li ng-repeat="value in entity.values"><a href="">{{value}}</a></li> </ul> </div> <span class="badge badge-info">{{entity.instanceCount}}</span> </h2> <blockquote> {{entity.rdfsComment}} </blockquote> <markdown md-src="docs/{{entity.typeName|cleanType:true}}.md" markdown-opts="{silent:true}"/> <div class="row"> <div class="col-xs-12 col-md-5 vertical-middle "> <div ng-repeat="parent in entity.parentTriples | filter : rdfHelp.triples.predicate"> <p><code><a href="#!/{{parent.subjectType|cleanType}}" class="text-info">{{parent.subjectType}}</a></code> &nbsp; <code class="text-warning">{{parent.predicate}}</code></span> </p> </div> </div> <div class="col-xs-12 col-md-7 form-inline"> <div class="panel panel-default"> <div class="panel-heading"><h5 class="text-center">{{entity.typeName}}</h5></div> <div class="panel-body"> <div ng-repeat="t in entity.triples | filter : rdfHelp.triples.predicate"> <code class="text-warning">{{t.predicate}}</code> <code><a class="text-success" href="#!/{{t.objectType|cleanType}}">{{t.objectType}}</a></code> &nbsp; <span class="label label-info">{{t.tripleCount}}</span> <select ng-if="t.literalType && t.values.length> 1" class="form-control"> <option ng-repeat="value in t.values" value="{{value}}">{{value}}</option> </select> <input ng-if="t.literalType && t.values.length==1" type="text" placeholder="{{t.values[0]}}" class="form-control"></input> </div> </div> </div> </div> </div> <div class="bs-docs-example"> <div ng-repeat="t in entity.triples | filter : rdfHelp.triples.predicate"> <code class="text-info">{{t.tripleSample}}</code> </div> </div> <div class="bs-docs-example"> <div ng-repeat="path in entity.pathToOrigin"> <code class="text-info">{{path}} ?statement</code> </div> </div> </section> </div> </div>');
-	$templateCache.put('html/np-help.md.html', '<markdown md-src="{{mdFile|trusted}}"/>');
+	$templateCache.put('html/np-help.element.html', '<div class="row offset1"> <div class="row"> <section id="{{entity.typeName}}"> <h2> {{entity.typeName|cleanType}}  <div ng-if="entity.values.length> 0" class="btn-group"> <button class="btn dropdown-toggle" data-toggle="dropdown">Values<span class="caret"></span> </button> <ul class="dropdown-menu"> <li ng-repeat="value in entity.values"><a href="">{{value}}</a></li> </ul> </div> <span class="badge badge-info">{{entity.instanceCount}}</span> </h2> <blockquote> {{entity.rdfsComment}} </blockquote> <div class="row"> <div class="col-xs-12 col-md-5 vertical-middle "> <div ng-repeat="parent in entity.parentTriples | filter : rdfHelp.triples.predicate"> <p><code><a href="#!/{{parent.subjectType|cleanType}}" class="text-info">{{parent.subjectType}}</a></code> &nbsp; <code class="text-warning">{{parent.predicate}}</code></span> </p> </div> </div> <div class="col-xs-12 col-md-7 form-inline"> <div class="panel panel-default"> <div class="panel-heading"><h5 class="text-center">{{entity.typeName}}</h5></div> <div class="panel-body"> <div ng-repeat="t in entity.triples | filter : rdfHelp.triples.predicate"> <code class="text-warning">{{t.predicate}}</code> <code><a class="text-success" href="#!/{{t.objectType|cleanType}}">{{t.objectType}}</a></code> &nbsp; <span class="label label-info">{{t.tripleCount}}</span> <select ng-if="t.literalType && t.values.length> 1" class="form-control"> <option ng-repeat="value in t.values" value="{{value}}">{{value}}</option> </select> <input ng-if="t.literalType && t.values.length==1" type="text" placeholder="{{t.values[0]}}" class="form-control"></input> </div> </div> </div> </div> </div> <div class="bs-docs-example"> <div ng-repeat="t in entity.triples | filter : rdfHelp.triples.predicate"> <code class="text-info">{{t.tripleSample}}</code> </div> </div> <div class="bs-docs-example"> <div ng-repeat="path in entity.pathToOrigin"> <code class="text-info">{{path}} ?statement</code> </div> </div> </section> </div> </div>');
+	$templateCache.put('html/np-help.md.html', '<markdown md-src="{{mdFile}}"></markdown>');
 }]);
